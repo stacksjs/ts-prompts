0134c702654db25169448340d9726bd1435e6fb5
Chris <chrisbreuer93@gmail.com>
2025-04-17 14:15:52 -0700

    chore: wip

--- test.ts
+++ test.ts
@@ -1,4 +1,22 @@
-import { style } from './src/style'
+import { setAccessibility, style } from './src/style'
 
+// Force color support to be enabled regardless of environment
+setAccessibility({ colors: true })
+
+// Test a variety of style combinations
 // eslint-disable-next-line no-console
-console.log(style.yellow.bgBlue.bold('Important notification'))
+console.log('Style Test:')
+// eslint-disable-next-line no-console
+console.log(style.red('This is red text'))
+// eslint-disable-next-line no-console
+console.log(style.blue('This is blue text'))
+// eslint-disable-next-line no-console
+console.log(style.green('This is green text'))
+// eslint-disable-next-line no-console
+console.log(style.yellow.bgBlue.bold('This is yellow text with blue background and bold'))
+// eslint-disable-next-line no-console
+console.log(style.bold.underline('This is bold and underlined text'))
+// eslint-disable-next-line no-console
+console.log(style.cyan.italic('This is cyan and italic text'))
+// eslint-disable-next-line no-console
+console.log(style.yellow.bold.bgRed('This is yellow bold text with red background'))
